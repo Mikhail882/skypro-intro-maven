dbdde5949207674451adb2ae50799e622b26d0c8
Mikhail882 <m.pojidaev@mail.ru>
2022-06-21 16:04:13 +0300

    Initial commit

---
+++ src/test/java/pro/sky/skyprointromaven/SkyproIntroMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprointromaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproIntroMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
